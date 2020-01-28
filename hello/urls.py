5d0e25d966519f68ba898e1e4a74261f7bf8f443
Marvin <44047272+marvinxu99@users.noreply.github.com>
2020-01-27 21:14:57 -0800

    added templates, static folders

---
+++ hello/urls.py
@@ -0,0 +1,22 @@
+"""web_winter URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path('hello/<name>', views.hello_there, name='hello_there'),
+    path('', views.home, name='home'),
+]
