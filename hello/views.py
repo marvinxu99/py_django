4fd5a8caca23f3427a62c434abc228a96ab2176b
Marvin <44047272+marvinxu99@users.noreply.github.com>
2020-01-28 19:21:37 -0800

    added log message model - update modles.py views.py urls.py

--- hello/views.py
+++ hello/views.py
@@ -2,10 +2,33 @@ import re
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.generic import ListView
+
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 
-def home(request):
-    return HttpResponse("Hello, Django!")
+# def home(request):
+#     #    return HttpResponse("Hello, Django!")
+#     return render(request, 'hello/home.html')
+
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+
+def about(request):
+    return render(request, 'hello/about.html')
+
+
+def contact(request):
+    return render(request, 'hello/contact.html')
 
 
 # def hello_there(request, name):
@@ -32,3 +55,16 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
